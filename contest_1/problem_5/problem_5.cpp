2a04244f037234bb9a7279e218237ed2bbda5cc3
Esya <56122211+cocoroko@users.noreply.github.com>
2020-11-23 02:52:26 +0300

    Update problem_5.cpp

--- contest_1/problem_5/problem_5.cpp
+++ contest_1/problem_5/problem_5.cpp
@@ -1,171 +1,180 @@
-//https://contest.yandex.ru/contest/19592/run-report/34593667/
 #include <iostream>
-#include <assert.h>
 
-class Stack {
- public:
-  Stack();
 
-  ~Stack();
-
-  void Push(int a); //обычный пуш
-  int Pop();  // не извлекает эллемент из стека
-  int Top();  // извлекает
-  bool IsEmpty() const { return top == -1; }
-
+struct Stack{
  private:
-  int *buffer;
-  int bufferSize;
-  int top;
+    int tail;
+    int capacity;
+    int* buffer;
+ public:
+    Stack();
+    ~Stack();
+    bool isEmpty();
+    void Push (int val);
+    int Pop();
+    int peek();
 
-  void grow(); //увеличение размерности массива при заполнении
 };
 
-int getPriority(char operand) { // получение приоритета операнда
-  if (operand == '*' || operand == '/') {
-    return 4;
-  }
-  if (operand == '+' || operand == '-') {
-    return 3;
-  }
-  if (operand == '(') {
-    return 2;
-  }
-  if (operand == ')') {
-    return 1;
-  }
-  return -1;
+Stack::Stack(){
+    tail = 0;
+    capacity = 8;
+    buffer = new int[capacity];
+}
+Stack::~Stack(){
+    delete[] buffer;
 }
 
-Stack::Stack() : bufferSize(2), top(-1) {
-  buffer = new int[bufferSize];
+bool Stack::isEmpty(){
+    return tail==0;
 }
 
-Stack::~Stack() {
-  delete[] buffer;
-}
-
-void Stack::Push(int a) {
-  if (top + 2 > bufferSize) {
-    grow();
-  }
-  //assert(top + 1 <  bufferSize);
-  buffer[++top] = a;
+void Stack::Push(int val){
+    if(tail == capacity - 1){
+        int* temp = new int[capacity * 2];
+        for(int i = 0; i < capacity; ++i){
+            temp[i] = buffer[i];
+        }
+        delete[] buffer;
+        buffer = temp;
+        capacity *= 2;
+    }
+    buffer[tail] = val;
+    tail++;
 }
 
 int Stack::Pop() {
-  assert(top != -1);
-  return buffer[top];
+    if(isEmpty()){
+        return -1;
+    }
+    int res = buffer[--tail];
+    return res;
 }
 
-int Stack::Top() {
-  assert(top != -1);
-  return buffer[top--];
+int Stack::peek() {
+    if(isEmpty()){
+        return -1;
+    }
+    return buffer[tail - 1];
 }
 
-void Stack::grow() {
-  int newBufferSize = bufferSize * 2;
-  int *tempBuffer = new int[newBufferSize];
-  for (int i = 0; i < bufferSize; ++i) {
-    tempBuffer[i] = buffer[i];
-  }
-  delete[] buffer;
-  buffer = tempBuffer;
-  bufferSize = newBufferSize;
+
+
+int getPriority (int action){
+    switch (action){
+        case -1 : return 1;
+        case -2 : return 1;
+        case -3 : return 2;
+        case -4 : return 2;
+    }
+    return 0;
 }
 
-int calculate(std::string outStr) {
-  Stack stack;
-  bool isNumberFirst = true;
-  int tempInt = 0;
-  int first = 0;
-  int second = 0;
-  for (int i = 0; i < outStr.length(); ++i) {
-    if (isdigit(outStr[i])) {
-      if (isNumberFirst) {
-        stack.Push(outStr[i] % 48); // из-за таблицы
-        isNumberFirst = false;
-      } else {
-        tempInt = stack.Top();
-        stack.Push(tempInt * 10 + outStr[i] % 48);
-      }
-    }
-    if (outStr[i] == ' ') { //по пробелу определяем что число закончилось
-      isNumberFirst = true;
-    }
-    if (outStr[i] == '+') {
-
-      first = stack.Top();
-      second = stack.Top();
-      stack.Push(first + second);
-    }
-    if (outStr[i] == '-') {
-      first = stack.Top();
-      second = stack.Top();
-      stack.Push(second - first);
-    }
-    if (outStr[i] == '/') {
-      first = stack.Top();
-      second = stack.Top();
-      stack.Push(second / first);
-    }
-    if (outStr[i] == '*') {
-      first = stack.Top();
-      second = stack.Top();
-      stack.Push(first * second);
-    }
-  }
-  return stack.Top();
-}
-
-int main() {
-  char symbol = '0'; //текущий элемент
-  std::string outStr;      // выходная строка в постфиксной нотации
-  bool isPreviousSymbolNumber = false; // является ли пред символ числом
-  Stack stackForCalculate; // стек для перевода
-  while (scanf("%c", &symbol) == 1) {
-    if (symbol == '\n' || symbol == '\0') {
-      break;
-    }
-    if (isdigit(symbol) != 0) {
-      isPreviousSymbolNumber = true; //ставим флаги
-      outStr.push_back(symbol); //заносим в выходную строку все числа
-    } else {
-      //if ( symbol == '(' || '+' || '-' || '*' || '/' || ')' )
-      if (isPreviousSymbolNumber) {
-        outStr.push_back(' '); //заносим пробелы после чисел
-        isPreviousSymbolNumber = false; // меняем флаги
-      }
-      if (symbol == '(' || symbol == ')') {
-        if (symbol == '(') {
-          stackForCalculate.Push(symbol); // ( всегда в стек
-        } else {
-          while (stackForCalculate.Pop() != '(') {
-            outStr.push_back(char(stackForCalculate.Top())); //извлекаем пока не найдем (
-          }
-          stackForCalculate.Top();
+int charToInt (char c){
+    switch (c){
+            case '+' :
+                return -1;
+            case '-' :
+                return -2;
+            case '*' :
+                return -3;
+            case '/' :
+                return -4;
+            case '(' :
+                return -5;
+            case ')' :
+                return -6;
+            default :
+                return c - (int)'0';
         }
-      } else {
-        if (stackForCalculate.IsEmpty()) {
-          stackForCalculate.Push(symbol); //если пуст просто кладем операнд
-        } else {
+}
 
-          while (!stackForCalculate.IsEmpty()) { // иначе
-            if (getPriority(char(stackForCalculate.Pop())) >= getPriority(symbol)) {
-              outStr.push_back(char(stackForCalculate.Top())); // если операнд ниже или равен приоритетом
-            } else {
-              break; //если выше
+int postfixCount(int* postfix, int postfixCapacity){
+    Stack* numbers = new Stack();
+    int current = 0;
+    for(int i = 0; i < postfixCapacity; ++i){
+        current = postfix[i];
+        if(current >= 0){
+           
+            numbers->Push(current);
+        }
+        else{
+            
+            int num1 = numbers->Pop();
+            int num2 = numbers->Pop();
+            int res = 0;
+            switch(current){
+                case -1 :
+                    res = num1 + num2;
+                    break;
+                case -2 :
+                    res = num2 - num1;
+                    break;
+                case -3 :
+                    res = num1 * num2;
+                    break;
+                case -4 :
+                    res = num2 / num1;
+                    break;
             }
-          }
-          stackForCalculate.Push(symbol); //все равно кладем в стек
+            numbers->Push(res);
         }
-      }
     }
-  }
-  while (!stackForCalculate.IsEmpty()) {
-    outStr.push_back(char(stackForCalculate.Top())); //заносим оставшиеся операторы в строку
-  }
-  std::cout << calculate(outStr); //считаем результат
+    int res =  numbers->Pop();
+    delete numbers;
+    return res;
+}
 
-  return 0;
+int InfToPost(const std::string& str) {
+    Stack* actions = new Stack();
+    int length = str.size();
+    int* postfix = new int[length];
+    int pos = 0;
+    int pCapacity = 0;
+    int curr = 0;
+    while(pos < length){
+        curr = charToInt(str[pos]);
+        if(curr >= 0){
+            int i = 1;
+            while(pos + i < length && isdigit(str[pos + i])){
+                curr = curr * 10 + charToInt(str[pos + i]);
+                i++;
+            }
+            pos += i - 1;
+        }
+        if(curr >= 0){
+            postfix[pCapacity++] = curr;
+        } else if(curr == -5){
+            actions->Push(curr);
+        } else if(curr == -6){
+            int upAction = actions->Pop();
+            while(upAction != -5){
+                postfix[pCapacity++] = upAction;
+                upAction = actions->Pop();
+            }
+        } else {
+            while(!actions->isEmpty()){
+                    if(getPriority(actions->peek()) < getPriority(curr)){
+                        break;
+                    }
+                    postfix[pCapacity++] = actions->Pop();
+            }
+            actions->Push(curr);
+        }
+        pos++;
+    }
+    while(!actions->isEmpty()){
+            postfix[pCapacity++] = actions->Pop();
+        }
+
+    delete actions;
+    return postfixCount(postfix, pCapacity);
+}
+
+
+int main(){
+    std::string input;
+    std::cin >> input;
+    std::cout << InfToPost(input);
+    return 0;
 }
