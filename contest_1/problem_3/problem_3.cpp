f3d24b1152b1d53cfb2d81a629d55325ce63a636
Esya <56122211+cocoroko@users.noreply.github.com>
2020-11-23 01:24:03 +0300

    Update problem_3.cpp

--- contest_1/problem_3/problem_3.cpp
+++ contest_1/problem_3/problem_3.cpp
@@ -1,12 +1,10 @@
 //https://contest.yandex.ru/contest/19592/run-report/34390679/
 #include <iostream>
 
-int *getarray(int n) {
-  int *arr = new int[n];
-  for (int i = 0; i < n; ++i) {
+void getarray(int* arr, int n) {
+  for (size_t i = 0; i < n; ++i) {
     std::cin >> arr[i];
   }
-  return arr;
 }
 
 int binsearch(int *arr, int n) {
@@ -14,9 +12,6 @@ int binsearch(int *arr, int n) {
   int right = n - 1;
   while (right != left) {
     int middle = (right + left) / 2;
-    //std::cout << "середина нач " << middle << " ";
-    //std::cout << "левый начало" << left << " ";
-    //std::cout << "правый начало" << right << std::endl;
     int prev = middle - 1;
     int next = middle + 1;
     if (right - left == 1) {
@@ -25,12 +20,9 @@ int binsearch(int *arr, int n) {
       else return left;
     } else if (arr[prev] < arr[middle] && arr[middle] < arr[next]) {
       left = middle;
-      //std::cout << "левый бок" << left << std::endl;
     } else if (arr[prev] > arr[middle] && arr[middle] > arr[next]) {
       right = middle;
-      //std::cout << "правый бок" << right << std::endl;
     } else if (arr[prev] < arr[middle] && arr[middle] > arr[next]) {
-      //std::cout << "середина - ответ" << middle << std::endl;
       return middle;
     }
   }
@@ -40,7 +32,9 @@ int binsearch(int *arr, int n) {
 int main() {
   int n;
   std::cin >> n;
-  int *arr = getarray(n);
+  int* arr = new int[n];
+  getarray(arr, n);
+  
   int pos = binsearch(arr, n);
   std::cout << pos << std::endl;
   delete[] arr;
