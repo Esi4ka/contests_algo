2e605d55cd89c17d290c0497981c5547dafddbe9
Esya <56122211+cocoroko@users.noreply.github.com>
2020-11-29 00:02:35 +0300

    Update problem_3.cpp

--- contest_1/problem_3/problem_3.cpp
+++ contest_1/problem_3/problem_3.cpp
@@ -3,19 +3,19 @@
 int Find(const int *arr, int n) {
  int inc = 1;
  while (inc < n) {
- if(arr[inc-1] < arr[inc]) {
- std::cout « inc « " ";
- inc *=2;
- std::cout « inc « " ";
- break;
- }
+   if(arr[inc-1] < arr[inc]) {
+     std::cout « inc « " ";
+     inc *=2;
+     std::cout « inc « " ";
+     break;
+   }
  return inc;
  }
 }
 
 void getarray(int* arr, int n) {
  for (size_t i = 0; i < n; ++i) {
- std::cin » arr[i];
+   std::cin » arr[i];
  }
 }
 
@@ -26,14 +26,14 @@ int binsearch(int* arr, int n) {
  
 
  while(true) {
- m = (left+right)/2;
- if(arr[m] > arr[m-1] && arr[m] > arr[m+1])
- break;
- if(arr[m] > arr[m-1]) {
- left = m;
- continue;
- }
- right = m;
+   m = (left+right)/2;
+   if(arr[m] > arr[m-1] && arr[m] > arr[m+1])
+     break;
+   if(arr[m] > arr[m-1]) {
+     left = m;
+     continue;
+   }
+   right = m;
  }
 
  std::cout « m;
@@ -46,14 +46,14 @@ int main() {
  getarray(arr, n);
 
  if(n == 2) {
- if(arr[0] > arr[1]) {
- std::cout « 0;
- return 0;
- }
+   if(arr[0] > arr[1]) {
+     std::cout « 0;
+     return 0;
+   }
  else {
- std::cout « 1;
- return 0;
- }
+   std::cout « 1;
+   return 0;
+   }
  }
  int line = Find(arr, n);
  std::cout « binsearch(arr, line) « std::endl;
