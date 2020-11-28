b4beb273d6b054029dbaaa6e25ab450da1fce354
Esya <56122211+cocoroko@users.noreply.github.com>
2020-11-29 00:01:02 +0300

    Update problem_3.cpp

--- contest_1/problem_3/problem_3.cpp
+++ contest_1/problem_3/problem_3.cpp
@@ -1,53 +1,62 @@
-//https://contest.yandex.ru/contest/19592/run-report/34390679/
 #include <iostream>
 
+int Find(const int *arr, int n) {
+ int inc = 1;
+ while (inc < n) {
+ if(arr[inc-1] < arr[inc]) {
+ std::cout « inc « " ";
+ inc *=2;
+ std::cout « inc « " ";
+ break;
+ }
+ return inc;
+ }
+}
+
 void getarray(int* arr, int n) {
-  for (size_t i = 0; i < n; ++i) {
-    std::cin >> arr[i];
-  }
+ for (size_t i = 0; i < n; ++i) {
+ std::cin » arr[i];
+ }
 }
 
 int binsearch(int* arr, int n) {
-  int left = 0;
-  int right = n;
-  int m = 0;
-  
-  if(n == 2) {
-      if(arr[0] > arr[1]) {
-          std::cout << 0;
-      }
-      else {
-          std::cout << 1;
-      }
-  }
-  else if (arr[0] > arr[1]) {
-      std::cout << 0;
-  }
-  else if(arr[n-1] > arr[n-2]) {
-      std::cout << n-1;
-  }
+ int left = 0;
+ int right = n;
+ int m = 0;
+ 
 
-  else {
-    while(true) {
-      m = (left+right)/2;
-      if(arr[m] > arr[m-1] && arr[m] > arr[m+1])
-          break;
-      if(arr[m] > arr[m-1]) {
-          left = m;
-          continue;
-      }
-      right = m;
-    }
-    std::cout << m;
-  }
+ while(true) {
+ m = (left+right)/2;
+ if(arr[m] > arr[m-1] && arr[m] > arr[m+1])
+ break;
+ if(arr[m] > arr[m-1]) {
+ left = m;
+ continue;
+ }
+ right = m;
+ }
+
+ std::cout « m;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  int* arr = new int[n];
-  getarray(arr, n);
-  std::cout << binsearch(arr, n) << std::endl;
-  delete[] arr;
-  return 0;
+ int n;
+ std::cin » n;
+ int* arr = new int[n];
+ getarray(arr, n);
+
+ if(n == 2) {
+ if(arr[0] > arr[1]) {
+ std::cout « 0;
+ return 0;
+ }
+ else {
+ std::cout « 1;
+ return 0;
+ }
+ }
+ int line = Find(arr, n);
+ std::cout « binsearch(arr, line) « std::endl;
+ delete[] arr;
+ return 0;
 }
